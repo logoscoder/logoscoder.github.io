8e83e65931d1e35b36823ed703539e9ac77b4ea3
Jessé Silva <36287920+logoscoder@users.noreply.github.com>
2019-06-29 23:54:51 -0300

    Update control.js

--- js/control.js
+++ js/control.js
@@ -3,7 +3,7 @@ window.onload = function(e){
     var randNumber = Math.floor((Math.random() * 100) + 1);
     var po = document.createElement('script'); 
     po.type = 'text/javascript'; 
-    po.src = "./js/main.js?vs="+ randNumber;
+    po.src = "./js/main.js";
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 };
